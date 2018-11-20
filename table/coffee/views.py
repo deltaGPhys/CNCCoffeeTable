feb57969118adea317012e5fccaa65fc1017be8b
Josh Gates <jgates@DeltaGPhysBook.local>
2018-11-19 19:42:28 -0500

    files working in pattern add

--- table/coffee/views.py
+++ table/coffee/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render_to_response, redirect, render
 from django.core.urlresolvers import reverse
+from django.views.decorators.csrf import csrf_exempt, ensure_csrf_cookie, csrf_protect
+
 import json
 
 from django.core.exceptions import ObjectDoesNotExist
@@ -16,17 +18,51 @@ from coffee.forms import *
 def IndexView(request):
     template_name = 'coffee/index.html'
 
-    patternaddform = PatternAddForm(prefix='patternadd')
 
-    context = {'patternaddform':patternaddform, }
-    return render(request,'coffee/index.html', context)
+    if request.method == 'GET':
 
+        patternaddform = PatternAddForm(prefix='patternadd')
+
+        context = {'patternaddform':patternaddform,}
+        return render(request,'coffee/index.html', context)
+
+    elif request.method == 'POST':
+
+        if "AddPattern" in request.POST:
+            f = PatternAddForm(request.POST,request.FILES,prefix='patternadd')
+            if f.is_valid():
+                new_pattern = f.save()
+                try:
+                    print new_pattern.Gcode.url
+                    print new_pattern.Photo.url
+                    print new_pattern.Blueprint.url
+                except Exception as e:
+                    print e
+
+            else:
+                print 'invalid'
+                print f.errors
+
+        return redirect(reverse('coffee:index'))
+
+
+
+
+@csrf_exempt
 def AddPatternView(request):
-    f = PatternAddForm(request.POST,prefix='patternadd')
-
+    f = PatternAddForm(request.POST,request.FILES,prefix='patternadd')
+    print request.POST
+    print request.FILES
     if f.is_valid():
+        gcode = request.FILES['Gcode']
+        new_pattern = f.save(commit=True)
+        try:
+            print new_pattern.Gcode.url
+            print new_pattern.Photo.url
+            print new_pattern.Blueprint.url
+        except Exception as e:
+            print e
 
-        new_pattern = f.save()
         return HttpResponse(json.dumps({"message": "Pattern Added"}), content_type="application/json", status=200)
 
     else:
@@ -41,7 +77,7 @@ def LoadPatternView(request):
         pattern = Pattern.objects.get(pk=id)
     except ObjectDoesNotExist:
         return HttpResponse(json.dumps({"message": "No such pattern"}), content_type="application/json", status=200)
-  
+
     response = {'Name':pattern.Name,
                 'Gcode':pattern.Gcode.url,
                 'Blueprint':pattern.Blueprint.url,
@@ -50,19 +86,19 @@ def LoadPatternView(request):
                 'Description':pattern.Description,
                 'Type':pattern.Type
                 }
-    print response
-    
+    #print response
+
     return HttpResponse(json.dumps(response), content_type="application/json", status=200)
 
 def CountPatternsView(request):
 
     n = Pattern.objects.all().count()
     pks = [x.pk for x in Pattern.objects.all()]
-    print n, pks
-    
+    #print n, pks
+
     response = {'Count':n,
                 'pks':pks
                 }
-    print response
-    
+    #print response
+
     return HttpResponse(json.dumps(response), content_type="application/json", status=200)
