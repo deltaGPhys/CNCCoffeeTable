ee166a1e2462ca46d3957e2e46aa55557b9a80c8
Josh Gates <jgates@DeltaGPhysBook.local>
2018-11-25 22:16:50 -0500

    .

--- table/coffee/views.py
+++ table/coffee/views.py
@@ -96,7 +96,7 @@ def SendLineView(request):
 
     line = request.POST.get('line')
     print line
-    line = line.strip().decode('utf-8')
+    line = bytes(line.strip(), encoding='UTF-8')
     print line
     print type(line)
 
